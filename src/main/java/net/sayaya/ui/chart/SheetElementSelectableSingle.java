13719445cb7e75ec924e3c4e8cf81c5cf38dc611
변상재 <sayaya@greencross.com>
2022-05-02 14:01:08 +0900

    .

--- src/main/java/net/sayaya/ui/chart/SheetElementSelectableSingle.java
+++ src/main/java/net/sayaya/ui/chart/SheetElementSelectableSingle.java
@@ -47,7 +47,7 @@ public interface SheetElementSelectableSingle extends HasSelectionChangeHandlers
 				TH.style.cursor = "pointer";
 				TH.innerHTML = "<input name='" + id + "' " +
 						"idx='" + data[row].idx() + "' " + (checked?"checked ":"") +
-						"type='radio' style='vertical-align: middle; margin: 0px;'/>";
+						"type='radio' style='vertical-align: middle; margin: 0px; cursor: pointer;'/>";
 			});
 		}).rowHeaderWidth(30);
 		sheetElement.element().addEventListener("click", evt->{
